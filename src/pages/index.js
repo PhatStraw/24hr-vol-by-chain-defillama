857d57c5813401096a639d0a0d075d22c6edf0ae
PhatStraw <kevindsimsjr@gmail.com>
2024-03-24 20:48:14 -0700

    hello

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,7 +3,7 @@ import BlockchainTVLChart from '../../components/tvlChart';
 export default function Home() {
   return (
     <div>
-    <h1>Blockchain TVL Chart</h1>
+    <h1>Blockchain Volume Chart</h1>
     <BlockchainTVLChart />
   </div>
   )
